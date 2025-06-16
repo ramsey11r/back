ba060b691e1efb4827d7bdb09072e6f159ee8e20
Your Name <you@example.com>
2025-06-16 16:56:03 +0100

    qsqsqs

--- backend/models/account.cs
+++ backend/models/account.cs
@@ -11,7 +11,7 @@
         public string besoin { get; set; }
         public string solution { get; set; }
         public string priorite { get; set; }
-        public string rr { get; set; }
+        public string rrfff { get; set; }
         
     }
 }
